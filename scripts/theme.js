93c2126cd18c0f26e2bbe94cea3c2e12e75be372
Diego <diegolima.almeida93@gmail.com>
2022-10-04 19:10:10 -0300

    add files

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,1 +1,32 @@
-/* Desenvolva sua lógica aqui ... */
+function changeTheme() {
+
+    let themeButton = document.getElementById("button-dk-mode")
+    let buttonIcon = document.getElementById("button-icon")
+    const html = document.querySelector("html")
+
+    themeButton.addEventListener("click", () => {
+
+        html.classList.toggle("dark-mode")
+        buttonIcon.classList.toggle("theme-changed")
+
+        const userPreference = localStorage.getItem("darkmode")
+
+        if (!userPreference) {
+
+            localStorage.setItem("darkmode", true)
+        } if (userPreference) {
+
+            localStorage.removeItem("darkmode")
+        }
+
+        if (buttonIcon.classList.contains("theme-changed")) {
+
+            buttonIcon.src = "../../assets/img/sun.svg"
+        } else {
+
+            buttonIcon.src = "../../assets/img/moon.svg"
+        }
+    })
+}
+
+changeTheme()
